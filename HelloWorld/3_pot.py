6ce55f1f20c4cedb726048f538078b358343a405
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2025-03-22 16:31:29 +0530

    pot

---
+++ HelloWorld/3_pot.py
@@ -0,0 +1,16 @@
+# All testing has been done in python 3.7.9
+from pinouts import PhyComPins as pins
+import pyfirmata
+from pyfirmata import Arduino, util
+
+from time import sleep
+times=5
+board = Arduino('COM3')
+
+it = util.Iterator(board)
+it.start()
+board.analog[pins.PIN_POT_1].enable_reporting()
+while 1:
+    _t=board.analog[pins.PIN_POT_1].read()
+    print(_t)
+    sleep(1)
