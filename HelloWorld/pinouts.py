fb62dc5031da089a0775832a8104e866d610bf39
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2025-03-22 15:58:18 +0530

    pinout added

---
+++ HelloWorld/pinouts.py
@@ -0,0 +1,31 @@
+class PhyComPins:
+    """Analog pins"""
+    PIN_POT_1       =0
+    PIN_POT_2       =1
+    PIN_POT_3       =2
+    PIN_THERMISTOR  =3
+    PIN_LAZER_DIODE =4   # output
+    PIN_LDR         =5
+    PIN_HALL        =6
+    PIN_IR_REC      =7
+
+    """Digital Pins"""
+    PIN_BUZZER=11
+    PIN_RGB_R=6
+    PIN_RGB_G=3
+    PIN_RGB_B=5
+    PIN_RGB_Y=2
+
+    PIN_TILT=12
+
+    PIN_BUTTON_1=4
+    PIN_BUTTON_2=7
+    PIN_BUTTON_3=13
+    PIN_BUTTON_4=8
+
+
+    PIN_SERVO_1=6
+    PIN_SERVO_2=5
+    PIN_SERVO_3=3
+    PIN_SERVO_4=10
+    PIN_SERVO_5=9
